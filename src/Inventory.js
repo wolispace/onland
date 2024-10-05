2f371b8f644d926c155e85309009a0e470c2ec9e
Wallace McGee <wm@wolispace.com>
2024-10-05 10:03:54 +0930

    start on inventory display

--- src/Inventory.js
+++ src/Inventory.js
@@ -14,13 +14,28 @@ class Inventory extends UniqueSet {
     this.dialog = new Dialog(params);
   }
 
+  // perry mason
+  // charlie chaplin
+
   html() {
     let html = '';
     this.list.forEach(itemId => {
-      const item = app.items.get(itemId);
-      html += `<li>${item.type}</li>`;
+      let item = app.items.get(itemId);
+      item.left = 0;
+      item.top = 0;
+      item.html = assets.buildHtml(item);
+      console.log(item);
+      html += `${this.buttonHtml(item)}`;
     });
-    return `<ul>${html}</ul>`;
+    return `<div class="itemList">${html}</div>`;
+  }
+
+  buttonHtml(item) {
+    let html = '';
+    html += `<div class="buttonize inventoryItem" data-item-id="${item.id}">${item.html}</div>`;
+
+    return html
+
   }
 
 }
