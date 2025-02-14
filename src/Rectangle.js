265f29a9e855d8acae7a0349de74a0ee2b2a702d
Wallace McGee <wm@wolispace.com>
2025-02-15 09:23:01 +1030

    updated rectanle functions now its pos and area

--- src/Rectangle.js
+++ src/Rectangle.js
@@ -5,17 +5,17 @@ import UniqueSet from './UniqueSet.js';
 
 export default class Rectangle {
   /**
-   * 
-   * @param {number} x 
-   * @param {number} y 
-   * @param {number} width 
-   * @param {number} height 
+   * @param {object} {x,y,w,h}
    */
   constructor(params) {
     this.position = new Point(params.x, params.y);    // Position using Point
     this.area = new Area(params.w, params.h); // Dimensions using Area
   }
 
+  /**
+   * 
+   * @returns Makes a copy of the current rectangle
+   */
   copy() {
     return new Rectangle(this);
   }
@@ -26,8 +26,8 @@ export default class Rectangle {
    */
   get center() {
     return new Point(
-      this.w / 2,
-      this.h / 2);
+      this.area.w / 2,
+      this.area.h / 2);
   }
 
   /**
@@ -36,29 +36,30 @@ export default class Rectangle {
    */
   corners() {
     const cornerList = new UniqueSet();
-    cornerList.add(new Point(this.x, this.y));
-    cornerList.add(new Point(this.x + this.w, this.y));
-    cornerList.add(new Point(this.x + this.w, this.y + this.h));
-    cornerList.add(new Point(this.x, this.y + this.h));
+    cornerList.add(new Point(this.position.x, this.position.y));
+    cornerList.add(new Point(this.position.x + this.area.w, this.position.y));
+    cornerList.add(new Point(this.position.x + this.area.w, this.position.y + this.area.h));
+    cornerList.add(new Point(this.position.x, this.position.y + this.area.h));
     return cornerList;
   }
 
   equals(rectangle) {
     return (
-      this.x === rectangle.x &&
-      this.y === rectangle.y &&
-      this.w === rectangle.w &&
-      this.h === rectangle.h
+      this.position.x === rectangle.x &&
+      this.position.y === rectangle.y &&
+      this.area.w === rectangle.w &&
+      this.area.h === rectangle.h
     );
   }
 
   /**
    * Multiply the area by the point for calculating offsets from a grid to the world coords
+   * With origin x,y remaining the same
    * @param {Area} area eg {w:5, h:2} 
    * @returns {Rectangle} eg: w,h=1000x1000 returns w:5000, h:2000
    */
   expand(area) {
-    return new Rectangle(this.w * area.w, this.h * area.h);
+    return new Rectangle(this.position.x, this.position.y, this.area.w * area.w, this.area.h * area.h);
   }
 
   /**
